06b3ec726684de0272242eee45f04e0c2f410c0b
Jeff Kelley <SlaunchaMan@gmail.com>
2014-05-04 14:12:11 -0400

    Initial Commit

---
+++ IntroToOpenGLESCode/DLGLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DLGLAppDelegate.h
+//  IntroToOpenGLESCode
+//
+//  Created by Jeff Kelley on 5/4/14.
+//  Copyright (c) 2014 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLGLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IntroToOpenGLESCode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IntroToOpenGLESCode
+//
+//  Created by Jeff Kelley on 5/4/14.
+//  Copyright (c) 2014 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DLGLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DLGLAppDelegate class]));
+    }
+}
